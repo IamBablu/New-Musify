89cbce3ccb87a8d181c00308fe05269d2131da33
Bablu Pandey <150371989+IamBablu@users.noreply.github.com>
2026-04-26 13:23:29 +0530

    Update script.js
    songs taking from supabase data base

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 console.log("Js Initializing....");
+const SUPABASE_URL = "https://nqtofvcwtmwovktqmvxd.supabase.co";
+const ANON_KEY = "sb_publishable_x3uExt9miHkGRSfUaWZUDQ_yrWj1WmX";
+const BUCKET = "New Musify";
 const left = document.querySelector(".left");
 const menu = document.querySelector(".menu");
 const cross = document.querySelector(".cross");
@@ -14,18 +17,46 @@ let currentDescription;
 let songs = [];
 let alboms = [];
 
+
+async function listFolder(prefix = "") {
+  try {
+    const res = await fetch(
+    `${SUPABASE_URL}/storage/v1/object/list/${encodeURIComponent(BUCKET)}`,
+    {
+      method: "POST", // GET nahi chalega Yuvraj, POST hi lagana padega
+      headers: {
+        apikey: ANON_KEY,
+        Authorization: `Bearer ${ANON_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prefix: prefix,
+        limit: 100,
+        offset: 0,
+      }),
+    },);
+  const data = await res.json();
+  if (data.error) throw new Error(data.message);
+  return data;
+  ;
+  } catch (error) {
+    console.log(error);
+  }
+  
+}
+
 // Gatting all songs from songs folder in an array
-async function gateSongs(folder) {
-  // let a = await fetch(`/songs/${folder}`);
-  let b = await fetch(`https://api.github.com/repos/IamBablu/New-Musify/tree/main/songs/${folder}`).then(res => res.json());
-  console.log("All songs: ",b)
-  songs = [];
-  Array.from(b).forEach(file => {
-    file.path.endsWith(".mp3")
-    console.log("NAYA FILE: ",file.path.replaceAll("%20", " ").split(`${folder}`)[1])
-    songs.push(file.path.replaceAll("%20", " ").split(`${folder}`)[1]);
-  })
-  // // Listing songs in library list
+async function gateSongs(e) {
+  try {
+    songs = [];
+  let b = await listFolder(`songs/${e}/`);
+  Array.from(b).forEach((file) => {
+    if (file.name.endsWith(".mp3")) {
+      file.name.endsWith(".mp3");
+      songs.push(file.name);
+    }
+  });
+  libraryList.innerHTML = "";
   songs.forEach((song) => {
     libraryList.innerHTML =
       libraryList.innerHTML +
@@ -39,7 +70,6 @@ async function gateSongs(folder) {
             </div>
             <img src="contant/play.svg" alt="" />
           </div>`;
-    // console.log(song.href.split("/songs/")[1].replaceAll("%20"," ").split("-")[0],song.href.split("/songs/")[1].replaceAll("%20"," ").split("-")[1])
   });
   //   Attach a event listener to all songs
   Array.from(document.querySelectorAll(".song")).forEach((e) => {
@@ -49,6 +79,9 @@ async function gateSongs(folder) {
   });
 
   return songs;
+  } catch (error) {
+    console.log(error);
+  }
 }
 function formatSeconds(seconds) {
   var minutes = Math.floor(seconds / 60);
@@ -64,61 +97,82 @@ function formatSeconds(seconds) {
   );
 }
 
-function playMusic(track, AudioPlay = true) {
-  currentSong.src = `${currentFolder}${track}`;
-  currentSongName = currentSong.src
-    .replaceAll("%20", " ")
-    .split(`${currentFolder}`)[1];
+function playMusic(track ="", AudioPlay = true) {
+  try {
+    currentSong.src = `https://nqtofvcwtmwovktqmvxd.supabase.co/storage/v1/object/public/New%20Musify/songs/${currentFolder}/${track}`;
+  currentSongName = track;
   if (AudioPlay) {
     currentSong.play();
-    // console.log(play);
     play.src = "contant/sickpause.svg";
   }
   ganaName.innerHTML = currentSongName;
   time.innerHTML = "00:00/00:00";
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // getting Banner of each alboms
 async function getBanner(e) {
-  let i = await fetch(`https://api.github.com/repos/IamBablu/New-Musify/contents/songs/${e}/`).then(res => res.json())
-  
-  Array.from (i).forEach((bimg)=>{
+  try {
+    let i = await listFolder(`songs/${e}/`);
+
+  for (let bimg of i) {
     if (
-      bimg.path.endsWith(".jpeg") ||
-      bimg.path.endsWith(".jpg") ||
-      bimg.path.endsWith(".png")
-    ){
-      currentBanner = bimg.path.replaceAll("%20"," ");
+      bimg.name.endsWith(".jpeg") ||
+      bimg.name.endsWith(".jpg") ||
+      bimg.name.endsWith(".png")
+    ) {
+      currentBanner = `songs/${e}/${bimg.name}`;
     }
-  })
+  }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // getting current title and description of folder
 async function getTitleDescription(folder) {
-  let t = await fetch(`https://api.github.com/repos/IamBablu/New-Musify/contents/songs/${folder}/info.json/`).then(res => res.json());
-  let response = await fetch(t.download_url).then(res => res.json())
-  
-  currentTitle = response.title;
-  currentDescription = response.description;
+  try {
+    const response = await listFolder(`songs/${folder}`);
+  for (let info of response) {
+    if (info.name.endsWith(".json")) {
+      let data = await (
+        await fetch(
+          `https://nqtofvcwtmwovktqmvxd.supabase.co/storage/v1/object/public/New%20Musify/songs/${folder}/${info.name}`,
+        )
+      ).json();
+      currentDescription = data.description;
+      currentTitle = data.title;
+    }
+  }
+  } catch (error) {
+    console.log(error);
+  }
 }
 // for showing folder in alboms
 async function showFolders() {
-   let f = await fetch('https://api.github.com/repos/IamBablu/New-Musify/contents/songs/').then(res => res.json());
-  
-  Array.from(f).forEach(e=>{
-    if(e.git_url.includes("trees")){
+  try {
+    const songsFolder = await listFolder("songs/");
+  Array.from(songsFolder).forEach((e) => {
+    if (e) {
       alboms.push(e.name);
-      
     }
-  })
-  
-  for (let index = 0; index < alboms.length; index++) {   
+  });
+  for (let index = 0; index < alboms.length; index++) {
+    if (alboms) {
       await getBanner(alboms[index]);
       await getTitleDescription(alboms[index]);
-      // Listing folder in alboms
-      cards.innerHTML += `<div data-folder = "${alboms[index]}" class="card">
+    } else {
+      console.log("missing Alboms");
+    }
+    let imgbanner = await fetch(
+      `https://nqtofvcwtmwovktqmvxd.supabase.co/storage/v1/object/public/New%20Musify/${currentBanner}`,
+    );
+    // Listing folder in alboms
+    cards.innerHTML += `<div data-folder = "${alboms[index]}" class="card">
       <div class="cardimg">
-        <img src="${currentBanner}" alt="" />
+        <img src="https://nqtofvcwtmwovktqmvxd.supabase.co/storage/v1/object/public/New%20Musify/${currentBanner}">
         <div class="playbtn">
           <img src="contant/playbtn.svg" alt="" />
           </div>
@@ -129,48 +183,59 @@ async function showFolders() {
           </div>
           </div>`;
   }
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function main() {
   await showFolders();
-  currentFolder = `${alboms[0]}/`;
-  await gateSongs(currentFolder);
-  // playMusic(songs[0], false);
-
-  // showing or hiding left container in small screen device
+  await gateSongs(alboms[0]);
   menu.addEventListener("click", () => {
     left.style.left = 0;
   });
   cross.addEventListener("click", () => {
     left.style.left = "-500px";
   });
-
+    
   //   Attach a event listener to play, next, previous button
   play.addEventListener("click", () => {
-    if (currentSong.paused) {
+    try {
+      if (currentSong.paused) {
       currentSong.play();
       play.src = "contant/sickpause.svg";
     } else {
       currentSong.pause();
       play.src = "contant/sickplay.svg";
     }
+    } catch (error) {
+      console.log(error);
+    }
   });
   previous.addEventListener("click", () => {
-    let index = songs.indexOf(currentSongName);
+    try {
+      let index = songs.indexOf(currentSongName);
     currentSong.pause();
     if (index > 0) {
       playMusic(songs[index - 1]);
     } else {
       playMusic(songs[index]);
+    } 
+    } catch (error) {
+      console.log(error)
     }
   });
   next.addEventListener("click", () => {
-    let index = songs.indexOf(currentSongName);
+    try {
+      let index = songs.indexOf(currentSongName);
     currentSong.pause();
     if (index < songs.length - 1) {
       playMusic(songs[index + 1]);
     } else {
       playMusic(songs[index]);
+    }
+    } catch (error) {
+      console.log(error)
     }
   });
 
@@ -178,7 +243,7 @@ async function main() {
   currentSong.addEventListener("timeupdate", () => {
     ganaName.innerHTML = currentSongName;
     time.innerHTML = `${formatSeconds(
-      currentSong.currentTime
+      currentSong.currentTime,
     )} / ${formatSeconds(currentSong.duration)}`;
     document.querySelector(".circle").style.left =
       (currentSong.currentTime / currentSong.duration) * 100 + "%";
@@ -193,11 +258,15 @@ async function main() {
 
   // Add event listener to Volume control
   volume.addEventListener("change", (e) => {
-    currentSong.volume = parseInt(e.target.value) / 100;
+    try {
+      currentSong.volume = parseInt(e.target.value) / 100;
     if (currentSong.volume == 0) {
       document.querySelector(".soundImg").src = "contant/soundstop.svg";
     } else {
       document.querySelector(".soundImg").src = "contant/soundplay.svg";
+    }
+    } catch (error) {
+      console.log(error)
     }
   });
 
@@ -216,14 +285,16 @@ async function main() {
   });
 
   document.querySelectorAll(".card").forEach((e) => {
-    e.addEventListener("click", async (element) => {
-      // console.log(element.currentTarget.dataset.folder);
+    try {
+      e.addEventListener("click", async (element) => {
       left.style.left = 0; //only in small screen device
-      currentFolder = "/songs/" + element.currentTarget.dataset.folder + "/";
-      // console.log(currentFolder);
+      currentFolder = element.currentTarget.dataset.folder;
       await gateSongs(currentFolder);
       playMusic(songs[0]);
     });
+    } catch (error) {
+      console.log(error)
+    }
   });
 }
 
